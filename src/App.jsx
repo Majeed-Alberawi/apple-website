57c61c989cbeda39135298a66b359cb58fd583c4
Majeed Alberawi <majeedmousa0@gmail.com>
2024-08-08 16:26:01 +0300

    finishing How It Works And create footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import Hightlights from "./components/Hightlights"
-import Model from "./components/Model"
-import Features from "./components/Features"
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Hightlights from "./components/Hightlights";
+import Model from "./components/Model";
+import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
+import Footer from "./components/Footer";
 
-import * as Sentry from '@sentry/react'
-import HowItWorks from "./components/HowItWorks"
+import * as Sentry from "@sentry/react";
 
 const App = () => {
   return (
@@ -16,8 +17,9 @@ const App = () => {
       <Model />
       <Features />
       <HowItWorks />
+      <Footer />
     </main>
-  )
-}
+  );
+};
 
 export default Sentry.withProfiler(App);
