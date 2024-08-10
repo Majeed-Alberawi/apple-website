e27fa5c3a648c00d16c0de5f178bd2ea910b2362
Majeed Alberawi <majeedmousa0@gmail.com>
2024-08-10 16:27:48 +0300

    Updating the formats

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,22 +1,22 @@
-import { useGSAP } from "@gsap/react"
+import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import ModelView from "./ModelView";
 import { useEffect, useRef, useState } from "react";
 import { yellowImg } from "../utils";
 
-import * as THREE from 'three';
+import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
 import { models, sizes } from "../constants";
 import { animateWithGsapTimeline } from "../utils/animations";
 
 const Model = () => {
-  const [size, setSize] = useState('small');
+  const [size, setSize] = useState("small");
   const [model, setModel] = useState({
-    title: 'iPhone 15 Pro in Natural Titanium',
-    color: ['#8F8A81', '#FFE7B9', '#6F6C64'],
+    title: "iPhone 15 Pro in Natural Titanium",
+    color: ["#8F8A81", "#FFE7B9", "#6F6C64"],
     img: yellowImg,
-  })
+  });
 
   // camera control for the model view
   const cameraControlSmall = useRef();
@@ -35,23 +35,23 @@ const Model = () => {
   const tl = gsap.timeline();
 
   useEffect(() => {
-    if(size === 'large') {
-      animateWithGsapTimeline(tl, small, smallRotation, '#view1', '#view2', {
-        transform: 'translateX(-100%)',
-        duration: 2
-      })
+    if (size === "large") {
+      animateWithGsapTimeline(tl, small, smallRotation, "#view1", "#view2", {
+        transform: "translateX(-100%)",
+        duration: 2,
+      });
     }
 
-    if(size ==='small') {
-      animateWithGsapTimeline(tl, large, largeRotation, '#view2', '#view1', {
-        transform: 'translateX(0)',
-        duration: 2
-      })
+    if (size === "small") {
+      animateWithGsapTimeline(tl, large, largeRotation, "#view2", "#view1", {
+        transform: "translateX(0)",
+        duration: 2,
+      });
     }
-  }, [size])
+  }, [size]);
 
   useGSAP(() => {
-    gsap.to('#heading', { y: 0, opacity: 1 })
+    gsap.to("#heading", { y: 0, opacity: 1 });
   }, []);
 
   return (
@@ -62,11 +62,15 @@ const Model = () => {
         </h1>
 
         <div className="flex flex-col items-center mt-5">
-          <div className={`w-full h-[75vh] md:h-[90vh] overflow-hidden relative cursor-grab ${isGrabbing ? 'cursor-grabbing' : 'cursor-grab'}`}
+          <div
+            className={`w-full h-[75vh] md:h-[90vh] overflow-hidden relative cursor-grab ${
+              isGrabbing ? "cursor-grabbing" : "cursor-grab"
+            }`}
             onMouseDown={() => setIsGrabbing(true)}
             onMouseUp={() => setIsGrabbing(false)}
-            onMouseLeave={() => setIsGrabbing(false)}>
-            <ModelView 
+            onMouseLeave={() => setIsGrabbing(false)}
+          >
+            <ModelView
               index={1}
               groupRef={small}
               gsapType="view1"
@@ -74,9 +78,9 @@ const Model = () => {
               setRotationState={setSmallRotation}
               item={model}
               size={size}
-            />  
+            />
 
-            <ModelView 
+            <ModelView
               index={2}
               groupRef={large}
               gsapType="view2"
@@ -89,14 +93,14 @@ const Model = () => {
             <Canvas
               className="w-full h-full"
               style={{
-                position: 'fixed',
+                position: "fixed",
                 top: 0,
                 bottom: 0,
                 left: 0,
                 right: 0,
-                overflow: 'hidden',
+                overflow: "hidden",
               }}
-              eventSource={document.getElementById('root')}
+              eventSource={document.getElementById("root")}
             >
               <View.Port />
             </Canvas>
@@ -108,13 +112,26 @@ const Model = () => {
             <div className="flex-center">
               <ul className="color-container">
                 {models.map((item, i) => (
-                  <li key={i} className="w-6 h-6 rounded-full mx-2 cursor-pointer" style={{ backgroundColor: item.color[0] }} onClick={() => setModel(item)} />
+                  <li
+                    key={i}
+                    className="w-6 h-6 rounded-full mx-2 cursor-pointer"
+                    style={{ backgroundColor: item.color[0] }}
+                    onClick={() => setModel(item)}
+                  />
                 ))}
               </ul>
 
               <button className="size-btn-container">
                 {sizes.map(({ label, value }) => (
-                  <span key={label} className="size-btn" style={{ backgroundColor: size === value ? 'white' : 'transparent', color: size === value ? 'black' : 'white'}} onClick={() => setSize(value)}>
+                  <span
+                    key={label}
+                    className="size-btn"
+                    style={{
+                      backgroundColor: size === value ? "white" : "transparent",
+                      color: size === value ? "black" : "white",
+                    }}
+                    onClick={() => setSize(value)}
+                  >
                     {label}
                   </span>
                 ))}
@@ -124,7 +141,7 @@ const Model = () => {
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Model
+export default Model;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
 //...
 import * as Sentry from "@sentry/react";
@@ -20,15 +20,15 @@ Sentry.init({
     }),
   ],
 
-  tracesSampleRate: 1.0, 
+  tracesSampleRate: 1.0,
   tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-  
+
   replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0, 
+  replaysOnErrorSampleRate: 1.0,
 });
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
