33a35720e88d8d0c3f12a09252e2474314c81d88
Majeed Alberawi <83315991+Majeed-Alberawi@users.noreply.github.com>
2025-09-28 11:14:59 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,7 +25,7 @@ const Footer = () => {
 
         <div className="flex md:flex-row flex-col md:items-center justify-between">
           <p className="font-semibold text-gray text-xs">
-            Copright @ 2024 Apple Inc. All rights reserved.
+            Copyright @ new Date().getFullYear(). Apple Inc. All rights reserved.
           </p>
           <div className="flex">
             {footerLinks.map((link, i) => (
